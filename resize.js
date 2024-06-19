b3085a6460354cf8574d95033049200576bbe827
MuffinShades <jamesweigand5@icloud.com>
2024-06-18 22:59:27 -0400

    hbajslkdjf

--- resize.js
+++ resize.js
@@ -1,1 +1,23 @@
 //maybe add this later idk
+//actually add this cause the css dont work, womp :( womp :((((((( lol
+(function() {
+    const t = window.querySelectorAll('[resizeable]'), types = ['horizontal', 'vertical', 'all'];
+
+    for (const e of t) {
+        var type = e.getAttribute('resizeType');
+
+        if (typeof type != 'string')
+            type = 'all';
+
+        for (var i in types)
+            if (types[i] == type) {
+                type = i;
+                break;
+            }
+
+        if (typeof type != 'number')
+            type = 2; //(all)
+
+        //create le drag element based on type
+    }
+})();
