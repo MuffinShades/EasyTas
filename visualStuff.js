69ef59759692e71ebfdd29eabdce2d5052ac0668
MuffinShades <89941934+MuffinShades@users.noreply.github.com>
2024-06-19 21:51:03 -0400

    added cool thingy on input menu :O

---
+++ visualStuff.js
@@ -0,0 +1,13 @@
+const tColors = ['#eee', '#fff'];
+
+function reloadTable() {
+    const targets = document.querySelectorAll('.tasInput');
+    var i=0,m;
+    
+    for (const e of targets) {
+        m = ++i % 2;
+        e.style.backgroundColor = tColors[m];
+    }
+}
+
+reloadTable();
